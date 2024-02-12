79b4f17a434fd6cfa2e77b6ee3d7035bda897878
Vijay-Subbu <159419410+Vijay-Subbu@users.noreply.github.com>
2024-02-12 07:35:19 +0000

    add some css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import './App.css'
 
 export default function App() {
   return (
-    <main><h1>HelloWorld</h1></main>
+    <main>
+      <h1>HelloWorld</h1>
+      <h3>Welcome All of You</h3>
+    </main>
       
       
    
